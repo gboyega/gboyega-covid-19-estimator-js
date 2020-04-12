9a74e5ec36c92816e92827885b59168811c0391a
gboyega <jaiyeolagboyega@gmail.com>
2020-04-12 19:46:12 +0100

    added frontend link

--- src/estimator.test.js
+++ src/estimator.test.js
@@ -53,21 +53,21 @@ const dollarsInFlight = getDollarsInFlight(
   'weeks'
 );
 
-console.log(
-  covid19ImpactEstimator({
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 4,
-      avgDailyIncomePopulation: 0.73
-    },
-    periodType: 'days',
-    timeToElapse: 38,
-    reportedCases: 2747,
-    population: 92931687,
-    totalHospitalBeds: 678874
-  })
-);
+// console.log(
+//   covid19ImpactEstimator({
+//     region: {
+//       name: 'Africa',
+//       avgAge: 19.7,
+//       avgDailyIncomeInUSD: 4,
+//       avgDailyIncomePopulation: 0.73
+//     },
+//     periodType: 'days',
+//     timeToElapse: 38,
+//     reportedCases: 2747,
+//     population: 92931687,
+//     totalHospitalBeds: 678874
+//   })
+// );
 
 test('getCurrentlyInfected function from Challenge1', () => {
   expect(currentlyInfected).toStrictEqual({
